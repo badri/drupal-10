5d62619e68dc0ae423f05cb4da6212b91d9f2e2c
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2023-11-17 12:00:40 +0530

    Add SB stack layer.

---
+++ web/sites/default/settings.shapeblock.php
@@ -0,0 +1,21 @@
+<?php
+use Drupal\Core\Installer\InstallerKernel;
+
+
+$config['system.logging']['error_level'] = 'verbose';
+
+  $databases['default']['default'] = [
+    'driver' => 'mysql',
+    'database' => getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'host' => getenv('DB_HOST'),
+    'port' => '3306',
+    'init_commands' => [
+      'isolation_level' => 'SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED',
+    ],
+  ];
+
+$settings['trusted_host_patterns'] = ['.*'];
+$settings['hash_salt'] = getenv('HASH_SALT');
+
